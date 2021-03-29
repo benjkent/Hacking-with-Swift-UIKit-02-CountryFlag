62dab706c57e3cd565c4d9b5b1d029aa88e4a63c
Benjamin Kent <benjaminfranklinkent@frontier.com>
2021-03-29 15:59:53 -0700

    game win loss

--- CountryFlag/ViewController.swift
+++ CountryFlag/ViewController.swift
@@ -16,7 +16,8 @@ class ViewController: UIViewController {
     var countries = [String]()
     var score: Int = 0
     var correctAnswer: Int = 0
-    
+    var winner: Bool = false
+    var loser: Bool = false
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -38,17 +39,18 @@ class ViewController: UIViewController {
     func askQuestion(action: UIAlertAction! = nil) {
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "SCORE: \(score)", style: .plain, target: self, action: #selector(scoreRules))
         
-        // randomly shuffle an array collection ** in place **
-        countries.shuffle()
         
-        button1.setImage(UIImage(named: countries[0]), for: .normal)
-        button2.setImage(UIImage(named: countries[1]), for: .normal)
-        button3.setImage(UIImage(named: countries[2]), for: .normal)
-        
-        correctAnswer = Int.random(in: 0...2)
-        
-        // set the title property in the navigaion bar
-        title = countries[correctAnswer].uppercased()
+            // randomly shuffle an array collection ** in place **
+            countries.shuffle()
+            
+            button1.setImage(UIImage(named: countries[0]), for: .normal)
+            button2.setImage(UIImage(named: countries[1]), for: .normal)
+            button3.setImage(UIImage(named: countries[2]), for: .normal)
+            
+            correctAnswer = Int.random(in: 0...2)
+            
+            // set the title property in the navigaion bar
+            title = countries[correctAnswer].uppercased()
     }
 
     @IBAction func buttonTapped(_ sender: UIButton) {
@@ -56,37 +58,73 @@ class ViewController: UIViewController {
         // Adjust the score
         // Show a correct or incorrect message.
         var title: String
-        
-        
+        var status: Bool
         if sender.tag == correctAnswer {
             title = "Correct"
             score += 1
             // Check to see if it is the winning score. 7
+            status = winOrLoss(currentScore: score)
         } else {
             title = "Incorrect that's \(countries[sender.tag].capitalized)'s flag"
             score -= 1
             // check to see if it is the losing score -7
+            status = winOrLoss(currentScore: score)
+        }
+        scoreResponse(title: title, status: status)
+        
+    }
+    func scoreResponse(title: String, status: Bool) {
+        if status == false {
+            let ac = UIAlertController(title: title, message: "Your Score is \(score).", preferredStyle: .alert)
+            
+            // WARNING: The handler askQuestion means - the name of the method to run
+            //                      askQuestion() means - run the askQuestion method right now.
+            // The style: has 3 value possiblilities. .default, .cancel, and .destructive
+            //            Each are different based on the IOS and have different user interface hints.
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+        } else if status == true && winner == true {
+            let ac = UIAlertController(title: "Winner", message: "Your Score is \(score).", preferredStyle: .alert)
+            
+            // WARNING: The handler askQuestion means - the name of the method to run
+            //                      askQuestion() means - run the askQuestion method right now.
+            // The style: has 3 value possiblilities. .default, .cancel, and .destructive
+            //            Each are different based on the IOS and have different user interface hints.
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+            gameReset()
+        } else {
+            let ac = UIAlertController(title: "Game Over", message: "Sorry, try again?", preferredStyle: .alert)
+            
+            // WARNING: The handler askQuestion means - the name of the method to run
+            //                      askQuestion() means - run the askQuestion method right now.
+            // The style: has 3 value possiblilities. .default, .cancel, and .destructive
+            //            Each are different based on the IOS and have different user interface hints.
+            ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
+            present(ac, animated: true)
+            gameReset()
         }
         
-        let ac = UIAlertController(title: title, message: "Your Score is \(score).", preferredStyle: .alert)
-        
-        // WARNING: The handler askQuestion means - the name of the method to run
-        //                      askQuestion() means - run the askQuestion method right now.
-        // The style: has 3 value possiblilities. .default, .cancel, and .destructive
-        //            Each are different based on the IOS and have different user interface hints.
-        ac.addAction(UIAlertAction(title: "Continue", style: .default, handler: askQuestion))
-        present(ac, animated: true)
     }
     @objc func scoreRules() {
         let ac = UIAlertController(title: "Score", message: "Get 7 correct, but 7 wrong and you lose", preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "got it", style: .default, handler: nil))
         present(ac, animated: true)
     }
-    func checkScore(currentScore: Int) -> Bool{
+    func winOrLoss(currentScore: Int) -> Bool{
         if currentScore == 7 {
+            winner = true
+            return true
+        } else if currentScore == -7{
+            loser = true
             return true
         }
         return false
+    }
+    func gameReset(){
+        winner = false
+        loser = false
+        score = 0
     }
 }
 
